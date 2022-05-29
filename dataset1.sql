65b556dba82c570cad729588afaf678b5c57dd1d
Ayushi <sinhaayushi272@gmail.com>
2022-05-29 15:41:54 +0530

    fight

---
+++ dataset1.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: face_recognition
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `covid`
+--
+
+DROP TABLE IF EXISTS `covid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `covid` (
+  `Profession` varchar(20) DEFAULT NULL,
+  `Place` varchar(45) DEFAULT NULL,
+  `Month` varchar(45) DEFAULT NULL,
+  `Year` varchar(45) DEFAULT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `Gender` varchar(45) DEFAULT NULL,
+  `Id` varchar(45) NOT NULL,
+  `Age` varchar(45) DEFAULT NULL,
+  `Place of death` varchar(45) DEFAULT NULL,
+  `Photo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `covid`
+--
+
+LOCK TABLES `covid` WRITE;
+/*!40000 ALTER TABLE `covid` DISABLE KEYS */;
+INSERT INTO `covid` VALUES ('Actor','Egypt','May','2021','Bikramjeet Kanwarpal','Male','1','52','India','No'),('Singer','Italy','March','2020','Detto Mariano','Male','2','82','Italy','No'),('Actor','France','May','2021','Sameer Ghanem','Male','3','84','Egypt','No');
+/*!40000 ALTER TABLE `covid` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-29 15:41:24
